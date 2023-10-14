5bc4c184d28997b79803c0e51c7b0bc3a436be3c
kameswari <vepadakameswari622@gmail.com>
2023-10-14 04:29:29 +0000

    solutions

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import RepositoryItem from './components/RepositoryItem'
+
+import GithubPopularRepos from './components/GithubPopularRepos'
+
+const App = () => <GithubPopularRepos />
+
+export default App
